d19ed5077385ca2282d82fca41000a485b0e160c
Cristian Bahamon <bahamon2803@gmail.com>
2022-02-02 03:00:39 -0500

    🛠️ update login's setting

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,31 +1,12 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { NavbarDefault } from '../ui/NavbarDefault';
-import { FormLogin } from './FormLogin';
+
 import { TestLogin } from './TestLogin';
 
 export const Login = () => {
 
-	const navigate = useNavigate();
-
-	const handleLogin = () => {
-		navigate('/profile', {
-			replace: true
-		})
-	}
-
 	return (
-		// <div className="container">
-		// 	<h1>Login</h1>
-		// 	<button
-		// 		className="btn btn-primary"
-		// 		onClick={handleLogin}
-		// 	>
-		// 		Login
-		// 	</button>
-		// </div>
+
 		<div className="container p-5">
-			{/* <FormLogin /> */}
 			<TestLogin />
 		</div>
 
