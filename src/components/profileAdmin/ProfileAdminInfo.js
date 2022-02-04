1ed1ca78e680ec3926fd62ddc24a9361311138ac
Cristian Bahamon <bahamon2803@gmail.com>
2022-02-04 01:01:40 -0500

    🛠️ Update desing users

--- src/components/profileAdmin/ProfileAdminInfo.js
+++ src/components/profileAdmin/ProfileAdminInfo.js
@@ -1,88 +1,14 @@
 import React from 'react'
 
-import { NavLink } from 'react-router-dom'
 import '../ui/modal/modal'
+
+import {DataUser} from "../ui/DataUser";
 
 export const ProfileAdminInfo = () => {
 
-	const user = {
-		photo: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-		nombres: 'Cristian Camilo',
-		apellidos: 'Bahamon Castaño',
-		correoI: 'u20182171994@usco.edu.co',
-		rol: 'Administrador',
-		pais: 'Colombia',
-		tipoDocumento: 'C.C',
-		numDocumento: '1193126103'
-
-	}
-
-
 	return (
 		<div className="">
-
-
-			<div className="bg-white shadow overflow-hidden sm:rounded-lg mt-6">
-				<div className="px-4 py-5 sm:px-6 flex justify-between">
-					<h3 className="text-lg leading-6 font-medium text-gray-900">Informacion Personal</h3>
-					<p className="mt-1 max-w-2xl text-sm text-gray-500"/>
-					<div className="ml-4 flex-shrink-0">
-						<NavLink to={`/profile-edit-info/1`} className="font-medium text-indigo-600 hover:text-indigo-500 " id="edit-btn">
-							Editar Informacion
-						</NavLink>
-						{/* <ProfileInfo user={user}/> */}
-						{/* <ModalEditProfile user={user}/> */}
-					</div>
-
-				</div>
-				<div className="border-t border-gray-200">
-					<dl>
-						<div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-							<dt className="text-sm font-medium text-gray-500">Nombres</dt>
-							<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{user.nombres}</dd>
-						</div>
-						<div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-							<dt className="text-sm font-medium text-gray-500">Apellidos</dt>
-							<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{user.apellidos}</dd>
-						</div>
-						<div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-							<dt className="text-sm font-medium text-gray-500">Corre Institucional</dt>
-							<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{user.correoI}</dd>
-						</div>
-						<div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-							<dt className="text-sm font-medium text-gray-500">Rol</dt>
-							<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{user.rol}</dd>
-						</div>
-						<div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-							<dt className="text-sm font-medium text-gray-500">Pais</dt>
-							<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-								{user.pais}
-							</dd>
-						</div>
-						<div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-							<dt className="text-sm font-medium text-gray-500">Documento</dt>
-							<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-								<ul role="list" className="border border-gray-200 rounded-md divide-y divide-gray-200">
-									<li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-										<div className="w-0 flex-1 flex items-center">
-											<img src="https://img.icons8.com/ios/20/000000/checked-identification-documents.png" alt='icon'/>
-											<span className="ml-2 flex-1 w-0 truncate">Tipo: {user.tipoDocumento}</span>
-										</div>
-									</li>
-									<li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-										<div className="w-0 flex-1 flex items-center">
-											<img src="https://img.icons8.com/ios-glyphs/20/000000/touch-id.png" alt='icon'/>
-											<span className="ml-2 flex-1 w-0 truncate">Numero: {user.numDocumento}</span>
-										</div>
-
-
-									</li>
-								</ul>
-							</dd>
-						</div>
-					</dl>
-				</div>
-			</div>
+			<DataUser />
 		</div>
 	)
 }
---
+++ src/components/ui/DataUser.js
@@ -0,0 +1,118 @@
+import React, {useEffect, useState} from 'react';
+import {NavLink} from "react-router-dom";
+import {fetchOneUser} from "../../service/service";
+
+export const DataUser = () => {
+    const myStorage = window.localStorage
+    const userId = myStorage.getItem("Id")
+    let oneUser=[];
+
+    const [dataUser, setDataUser] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const data = async () => {
+        setLoading(true)
+        try {
+            oneUser = await fetchOneUser(userId);
+            setDataUser(oneUser.data);
+            console.log(oneUser.data)
+            setLoading(false)
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        try {
+            data()
+        }catch (e) {
+            console.log(e)
+        }
+    }, []);
+
+    return (
+        <div className="">
+            <div className="bg-white shadow overflow-hidden sm:rounded-lg mt-6">
+                <div className="px-4 py-5 sm:px-6 flex justify-between">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">Informacion Personal</h3>
+                    <p className="mt-1 max-w-2xl text-sm text-gray-500"/>
+                    <div className="ml-4 flex-shrink-0">
+                        <NavLink to={`/profile-edit-info/${userId}`} className="font-medium text-indigo-600 hover:text-indigo-500 " id="edit-btn">
+                            Editar Informacion
+                        </NavLink>
+                    </div>
+
+                </div>
+                <div className="border-t border-gray-200">
+                    <dl>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Nombres</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{dataUser.firstname}</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Apellidos</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{dataUser.lastname}</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Correo</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{dataUser.email}</dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Corre Institucional</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{dataUser.institutionalEmail}</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Rol</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{dataUser.role?.label || ''}</dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Pais</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                {dataUser.country?.name || ''}
+                            </dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Documento de Identidad</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                <ul role="list" className="border border-gray-200 rounded-md divide-y divide-gray-200">
+                                    <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                        <div className="w-0 flex-1 flex items-center">
+                                            <img src="https://img.icons8.com/ios/20/000000/checked-identification-documents.png" alt='icon'/>
+                                            <span className="ml-2 flex-1 w-0 truncate">Tipo: {dataUser.documentType?.name || ''}</span>
+                                        </div>
+                                    </li>
+                                    <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                        <div className="w-0 flex-1 flex items-center">
+                                            <img src="https://img.icons8.com/ios-glyphs/20/000000/touch-id.png" alt='icon'/>
+                                            <span className="ml-2 flex-1 w-0 truncate">Numero: {dataUser.documentNumber}</span>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Genero</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                {dataUser.gender?.name || ''}
+                            </dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Grupo Etnico</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                {dataUser.ethnicGroup?.name || ''}
+                            </dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Estado Civil</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                {dataUser.civilStatus?.name || ''}
+                            </dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
