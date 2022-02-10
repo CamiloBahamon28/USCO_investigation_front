b604c3fc92b960a3b1bac5cee91bdea8c218a9ee
Cristian Bahamon <bahamon2803@gmail.com>
2022-02-10 11:29:33 -0500

    🆕 Create views' CRUD of 'Area No Educativa'

--- src/components/profileAdmin/Controls/areaNoEducative/CrudAreaNoEducative.js
+++ src/components/profileAdmin/Controls/areaNoEducative/CrudAreaNoEducative.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Load} from "../../../ui/Load";
+import {NavLink} from "react-router-dom";
 
 export const CrudAreaNoEducative = ({areas, loading}) => {
 
@@ -8,8 +9,15 @@ export const CrudAreaNoEducative = ({areas, loading}) => {
     }
 
     return (
-        <div>
-            <div className="flex flex-col">
+        <div className="container mx-auto">
+            <div className="flex flex-col ">
+                <div className="flex justify-end">
+                    <NavLink to={`/are-no-educative/new`}
+                             className="font-medium text-indigo-600 hover:text-indigo-500 p-3"
+                             id="edit-btn">
+                        Crear Area
+                    </NavLink>
+                </div>
                 <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
@@ -17,7 +25,7 @@ export const CrudAreaNoEducative = ({areas, loading}) => {
                                 <thead className="bg-gray-50 divide-y divide-gray-200">
                                 <tr>
                                     <th
-                                        colSpan={5}
+                                        colSpan={6}
                                         className="px-6 py-3 text-center
                                          text-2xl font-medium text-gray-500 uppercase tracking-wider ">
                                         Area No Educativa
@@ -55,6 +63,12 @@ export const CrudAreaNoEducative = ({areas, loading}) => {
                                     >
                                         Estado
                                     </th>
+                                    <th
+                                        scope="col"
+                                        className=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Acciones
+                                    </th>
                                 </tr>
                                 </thead>
                                 <tbody className="bg-white divide-y divide-gray-200">
@@ -74,6 +88,23 @@ export const CrudAreaNoEducative = ({areas, loading}) => {
                                         </td>
                                         <td className="px-6 py-4 whitespace-nowrap">
                                             <div className="text-sm text-gray-900">{area.status}</div>
+                                        </td>
+                                        <td className="">
+                                            <NavLink
+                                                to={`/are-no-educative/${area.id}`}
+                                                // to={`/profile-edit-info/${person.id}`}
+                                                className="mr-3 inline-flex items-center px-3 py-2 border border-indigo-600 rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                            >
+                                                Editar
+                                            </NavLink>
+
+                                            <button
+                                                type='submit'
+                                                className="inline-flex items-center px-3 py-2 border border-red-500 rounded-md shadow-sm text-sm font-medium text-white bg-red-500 hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                                // onClick={() => handleDeleteUser(person.id)}
+                                            >
+                                                Borrar
+                                            </button>
                                         </td>
 
                                     </tr>
